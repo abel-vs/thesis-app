86bc44e28b71af182eb26f0c6c832d7d292a6183
abel <a.vansteenweghen@student.tudelft.nl>
2023-01-19 13:54:08 +0000

    Change text and delete links

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,77 +6,50 @@ const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Thesis</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+           Tool {' '}
+          <a className="text-green-600">
+            X
           </a>
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
+          Automated model compression for deep learning.
         </p>
 
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+        <div className="mt-6 mb-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
+
+          <a className="mt-6 w-96 rounded-xl border p-6 text-left hover:bg-gray-100">
+            <h3 className="text-2xl font-bold">Model State &rarr;</h3>
             <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
+              This file contains your trained model.
+              It should be a 
+              <code className='rounded-md bg-gray-100 p-1 m-1 font-mono text-lg'>.pt</code> or <code className='rounded-md bg-gray-100 p-1 m-1 font-mono text-lg'>.pth</code> file.
             </p>
           </a>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
+          <a className="mt-6 w-96 rounded-xl border p-6 text-left hover:bg-gray-100">
+            <h3 className="text-2xl font-bold">Model Architecture &rarr;</h3>
             <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+              This file contains the architecture of your model.
+              It should be a
+              <code className='rounded-md bg-gray-100 p-1 m-1 font-mono text-lg'>.py</code> file.
             </p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
-      </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      </main>
+ 
+
+      <footer className="flex h-24 mt-6 w-full items-center justify-center border-t">
+        <a className="flex items-center justify-center gap-2">
+          Thesis Project by Abel Van Steenweghen
         </a>
       </footer>
     </div>
