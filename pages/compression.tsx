fc13a9ebfcba556076e2c04400c7ef81a071dcf0
Abel Van Steenweghen <abel.van.steenweghen@gmail.com>
2023-04-01 17:28:45 -0400

    Fix compression selection

--- pages/compression.tsx
+++ pages/compression.tsx
@@ -27,8 +27,9 @@ const SelectableCard = ({ children, className, onClick }) => {
   const [selected, setSelected] = useState(true);
   return (
     <Card
-      className={`${className} text-left relative outline outline-offset-0 outline-none rounded-md hover:bg-gray-100 ${selected ? 'outline-4 outline-green-500 shadow-lg' : ''
-        }`}
+      className={`${className} text-left relative outline outline-offset-0 outline-none rounded-md hover:bg-gray-100 ${
+        selected ? 'outline-4 outline-green-500 shadow-lg' : ''
+      }`}
       onClick={() => {
         setSelected(!selected);
         onClick(!selected);
@@ -40,6 +41,28 @@ const SelectableCard = ({ children, className, onClick }) => {
   );
 };
 
+const ActionCard = ({ action, onClick }) => {
+  return (
+    <SelectableCard
+      className="m-6"
+      key={action.name}
+      onClick={onClick}
+    >
+      <h3 className="text-2xl font-bold flex flex-row items-center capitalize">
+        <CompressionIcon type={action.type} />
+        {action.type}
+      </h3>
+      <p className="mt-4">
+        Suggested method: <Code>{action.name}</Code>
+      </p>
+      Settings: {<Code>{JSON.stringify(action.settings)}</Code>}
+    </SelectableCard>
+  );
+};
+
+
+
+
 const CompressionPage: NextPage = () => {
   const context = useContext(AppContext);
   const [loading, setLoading] = useState(false);
@@ -48,7 +71,19 @@ const CompressionPage: NextPage = () => {
   if (context.compressionActions === undefined) {
     router.push('/');
     return;
-  }
+  } 
+
+  const [selectedActions, setSelectedActions] = useState(context.compressionActions as Object[]) ;
+
+  const removeAction = (value: Object) => {
+    setSelectedActions(actions => actions.filter(element => element !== value));
+  };
+
+  const addAction = (action: Object) => {
+    setSelectedActions(actions => [...actions, action]);
+  };
+  
+
 
   return (
     <>
@@ -60,23 +95,15 @@ const CompressionPage: NextPage = () => {
       <div className="my-6 flex flex-wrap items-center justify-around">
         {context.compressionActions === null
           ? 'No actions suggested'
-          : context.compressionActions.map((action) => (
-            <SelectableCard
-              className="w-80 m-6"
-              key={action.name}
-              onClick={(selected: boolean) => {
-                action.selected = selected;
-              }}
-            >
-              <h3 className="text-2xl font-bold flex flex-row items-center capitalize">
-                <CompressionIcon type={action.type} />
-                {action.type}
-              </h3>
-              <p className="mt-4">
-                Suggested method: <Code text={action.name} />
-              </p>
-            </SelectableCard>
-          ))}
+          : context.compressionActions.map((x) => <ActionCard action={x} onClick={
+            (selected: boolean) => {
+              if(selected){
+                addAction(x)
+              } else{
+                removeAction(x)
+              }
+            }
+          }/>)}
 
         <div className="flex flex-row w-full m-8">
           <Link href="/goal" className="flex-none w-10 mr-2">
@@ -86,17 +113,18 @@ const CompressionPage: NextPage = () => {
           <Button
             text="Compress"
             loading={loading}
+            disabled={selectedActions.length === 0}
             onClick={async () => {
               setLoading(true);
-              const res = await compressModel(
-                context.modelStateFile,
-                context.modelArchitectureFile,
-                {
-                  actions: context.compressionActions.filter((action) => action.selected),
-                  dataset: context.dataset,
-                }
-              );
-              if (res == undefined){
+              const res = await compressModel(context.modelStateFile, context.modelArchitectureFile, {
+                actions: selectedActions,
+                // actions: context.compressionActions.filter((action) => action.selected),
+                dataset: context.dataset,
+                performance_target: context.performanceTarget,
+                compression_type: context.compressionType,
+                compression_target: context.compressionTarget,
+              });
+              if (res == undefined) {
                 setLoading(false);
                 setErrorMessage('Something went wrong on the server. Please try again later. ');
               } else if (!res.ok) {
@@ -109,6 +137,7 @@ const CompressionPage: NextPage = () => {
                 context.setOriginalResults(data.original_results);
                 context.setCompressedResults(data.compressed_results);
                 context.setCompressedFile(data.compressed_model);
+                context.setCompressedArchitecture(data.compressed_architecture);
                 setLoading(false);
                 router.push('/results');
               }
