57016f2e19c3be724e5848494aaed808596cad86
Abel Van Steenweghen <abel.van.steenweghen@gmail.com>
2023-06-12 14:48:11 +0100

    Fix 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,14 @@
-export default function Custom404() {
-  return null;
-}
+import { useRouter } from 'next/router';
+import Button from '../components/Button';
 
-export const getStaticProps = () => {
-  return {
-    redirect: {
-      destination: '/',
-    },
-  };
-};
+export default function Custom404() {
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-gray-900">
+      <h1 className="text-3xl font-bold">404 - Page Not Found</h1>
+      <p className="mt-3">The page you are looking for doesn`&apos;`t exist.</p>
+      <Button text="Go Home" onClick={() => router.push('/')} />
+    </div>
+  );
+}
